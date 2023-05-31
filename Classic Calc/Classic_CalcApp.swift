f7fd2fb4ad4d48843196644d59b9087fdd2c5e55
Graham Hall <graham@ghall.dev>
2023-05-31 10:26:49 -0400

    Initial Commit

---
+++ Classic Calc/Classic_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Classic_CalcApp.swift
+//  Classic Calc
+//
+//  Created by Graham Hall on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Classic_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
